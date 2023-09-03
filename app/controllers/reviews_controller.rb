56c92a6c40b90d7226ff1e7d756cbefb0981fc03
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-03 14:39:11 +0300

    Done lab

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,12 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def show
+    review = Review.find_by(id: params[:id])
+    render json: review, include: :dog_house
   end
 
 end
